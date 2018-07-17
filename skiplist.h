e79d26b7571a643cbab7840a9e99b8cc3e276586
runtianchen <34415085+runtianchen@users.noreply.github.com>
2018-07-17 23:57:26 +0800

    Add files via upload

--- skiplist.h
+++ skiplist.h
@@ -21,12 +21,20 @@ typedef struct zskiplist
 	int level;
 } zskiplist;
 
+//创建一个新的跳跃表
 zskiplist* zslCreate();
+//释放给定跳跃表，以及表中包含的所有节点
 void zslFree(zskiplist**);
+//将包含给定值的节点添加到跳跃表中
 int zslInsert(zskiplist*, int);
+//展示跳跃表中所有元素
 void zslGetAll(zskiplist*);
+//返回指定值的节点的排位
 int zslGetRank(zskiplist*, int);
+//通过排位返回指定节点
 int zslGetByRank(zskiplist*, int);
+//删除指定值的节点
 zskiplistNode* zslDel(zskiplist*, int);
+//通过排位删除指定节点
 zskiplistNode* zslDelByRank(zskiplist* ,int);
 #endif
